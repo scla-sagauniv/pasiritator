afc79f9b9a41af0553c6390247704959c0a4d3fb
shunsuke-tamura <wwwxx4869@gmail.com>
2023-01-21 23:58:48 +0900

    refuctor: prettier

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,22 @@
 <script>
-  import Button from '../lib/components/Button.svelte';
-  import axios from 'axios';
-  // require('dotenv').config();
-  // console.log(process.env.KEY1);  //=> VALUE1
-  // console.log(process.env.KEY2);  //=> VALUE2
-  // console.log(process.env.KEY3);  //=> VALUE3
+	import Button from '../lib/components/Button.svelte';
+	import axios from 'axios';
+	// require('dotenv').config();
+	// console.log(process.env.KEY1);  //=> VALUE1
+	// console.log(process.env.KEY2);  //=> VALUE2
+	// console.log(process.env.KEY3);  //=> VALUE3
 
+	const fetchContributions = async (userId) => {
+		const now = new Date();
+		const to = now.toISOString();
+		now.setFullYear(now.getFullYear() - 1);
+		const from = now.toISOString();
 
-  const fetchContributions = async (userId) => {
-    const now = new Date()
-    const to = now.toISOString();
-    now.setFullYear(now.getFullYear() - 1)
-    const from = now.toISOString();
-
-    // const query = `
-    // { "query": "query { viewer { login }}"}
-    // `
-    const query = {
-      "query": `query contributions {
+		// const query = `
+		// { "query": "query { viewer { login }}"}
+		// `
+		const query = {
+			query: `query contributions {
           user(login: "${userId}") {
             contributionsCollection(from: "${from}", to: "${to}") {
               contributionCalendar {
@@ -31,22 +30,20 @@
             }
           }
         }`
-    };
-    console.log(query);
-    
-    const token = import.meta.env.VITE_KEY1
-    const headers = {
-      'Authorization': `bearer ${token}`,
-      'Content-type': 'application/json'
-    }
-    const res = await axios.post('https://api.github.com/graphql', query, {headers: headers});
-    console.log(res.data);
-    
-  }
-  // const arrayEdit = async (readata) => {
-  // }
-  let name = "";
-  
+		};
+		console.log(query);
+
+		const token = import.meta.env.VITE_KEY1;
+		const headers = {
+			Authorization: `bearer ${token}`,
+			'Content-type': 'application/json'
+		};
+		const res = await axios.post('https://api.github.com/graphql', query, { headers: headers });
+		console.log(res.data);
+	};
+	// const arrayEdit = async (readata) => {
+	// }
+	let name = '';
 </script>
 
 <svelte:head>
@@ -54,43 +51,49 @@
 </svelte:head>
 
 <section>
-  <h1>Grass Base</h1>
-  <h2>Githubアカウントにログイン</h2>
-  <form>
-    <input class="github-acc" type="text" name="github-account" bind:value={name} placeholder="Github ID">
-  </form>
-    <Button onClick={() => fetchContributions(`${name}`)} userId={name}/>
+	<h1>Grass Base</h1>
+	<h2>Githubアカウントにログイン</h2>
+	<form>
+		<input
+			class="github-acc"
+			type="text"
+			name="github-account"
+			bind:value={name}
+			placeholder="Github ID"
+		/>
+	</form>
+	<Button onClick={() => fetchContributions(`${name}`)} userId={name} />
 </section>
 
 <style>
-  	section {
+	section {
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
 		flex: 0.6;
 	}
-  h1 {
-    font-size: 84px;
-    color: #FEBD69;
-    margin: 20px 0;
-  }
-  h2 {
-    font-size: 24px;
-    color: #fff;
-    margin: 0;
-  }
-  form {
-    margin-top: 20px;
-    width: 100%;
-    display: flex;
+	h1 {
+		font-size: 84px;
+		color: #febd69;
+		margin: 20px 0;
+	}
+	h2 {
+		font-size: 24px;
+		color: #fff;
+		margin: 0;
+	}
+	form {
+		margin-top: 20px;
+		width: 100%;
+		display: flex;
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
-  }
-  .github-acc {
-    margin: 0 auto;
-    padding: 10px 30px;
-    width: 250px;
-  }
+	}
+	.github-acc {
+		margin: 0 auto;
+		padding: 10px 30px;
+		width: 250px;
+	}
 </style>
