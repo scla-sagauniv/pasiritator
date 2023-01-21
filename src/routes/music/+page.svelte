2ecb5f1d622a4b6b510d6edbe734380407fc6e24
shunsuke-tamura <wwwxx4869@gmail.com>
2023-01-21 16:03:47 +0900

    update: UI作成

--- src/routes/music/+page.svelte
+++ src/routes/music/+page.svelte
@@ -9,6 +9,11 @@
 	);
 	console.log(music.score);
 
+	let isPlaying = false;
+	const togglePlaying = () => {
+		isPlaying = !isPlaying;
+	};
+
 	let onPlay = () => {};
 	let onStop = () => {};
 
@@ -26,15 +31,80 @@
 		});
 		onPlay = async () => {
 			await sampler.context.resume();
+			togglePlaying();
 			Tone.Transport.start();
 		};
 		onStop = async () => {
+			togglePlaying();
 			Tone.Transport.pause();
 		};
 	});
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<button on:click={onPlay}>start</button>
-<button on:click={onStop}>stop</button>
+<div class="full">
+	{#if !isPlaying}
+		<div class="video_play" on:click={onPlay} />
+	{:else}
+		<div class="video_pause" on:click={onStop} />
+	{/if}
+</div>
+
+<style>
+	.full {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		text-align: center;
+		width: 100%;
+		min-height: 100vh;
+	}
+	.video_play {
+		display: inline-block;
+		position: relative;
+		width: 1em;
+		height: 1em;
+		border: 0.05em solid currentColor;
+		border-radius: 50%;
+		color: #febd69;
+		font-size: 200px;
+	}
+	.video_play::before {
+		position: absolute;
+		top: 50%;
+		left: 30%;
+		transform: translateY(-50%);
+		width: 0px;
+		height: 0px;
+		border: 0.3em solid transparent;
+		border-left: 0.5em solid currentColor;
+		box-sizing: border-box;
+		content: '';
+	}
+	.video_pause {
+		display: inline-block;
+		position: relative;
+		width: 1em;
+		height: 1em;
+		border: 0.05em solid currentColor;
+		border-radius: 50%;
+		color: #febd69;
+		font-size: 200px;
+	}
+	.video_pause::before,
+	.video_pause::after {
+		position: absolute;
+		top: 50%;
+		transform: translateX(-50%) translateY(-50%);
+		width: 0.1em;
+		height: 0.5em;
+		box-sizing: border-box;
+		background-color: currentColor;
+		content: '';
+	}
+	.video_pause::before {
+		left: 40%;
+	}
+	.video_pause::after {
+		left: 60%;
+	}
+</style>
